9f27e50ce2ad76f6f7841f774c243db8367e27a5
Awinja-j <ingari61@hotmail.com>
2021-02-24 17:00:53 +0300

    new changes

--- app.py
+++ app.py
@@ -15,8 +15,12 @@ def test_is_boolean(s):
 def test_is_dates(s):
     print(s)
     assert s == s
-
-@given(st.integers(min_value=0, max_value=10))
+g=st.lists(st.one_of(st.integers(),st.just('joan'), st.none()))
+h=st.dates()
+n={"a":g}
+m={"p":h}
+l=st.fixed_dictionaries(n, optional=m).map(lambda x: anything(x['a'], x['p'] if 'p' in x else 90) )
+@given(l)
 def test_is_integers(s):
     print(s)
     assert s == s
@@ -26,10 +30,18 @@ def test_is_list(s):
     print(s)
     assert s == s
 
+class anything:
+    def __init__(self, d, b):
+        self.d=d
+        self.b=b
+
+
+
+
 
 if __name__ == '__main__':
-    test_is_datetime()
-    test_is_list()
+    # test_is_datetime()
+    # test_is_list()
     test_is_integers()
-    test_is_dates()
-    test_is_boolean()
+    # test_is_dates()
+    # test_is_boolean()
